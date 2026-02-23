9faa11cdd60b815823d7282b8518ec11f4ce124e
MasfikurDev <iammasfikur@gmail.com>
2026-02-24 00:21:21 +0600

    feat: added path aliases for improved module resolution in TypeScript and Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@types": path.resolve(__dirname, "./src/types"),
+            "@lib": path.resolve(__dirname, "./src/lib"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@contexts": path.resolve(__dirname, "./src/contexts"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@ui" : path.resolve(__dirname, "./src/components/ui"),
+
+            "@app": path.resolve(__dirname, "./src/app"),
+            "@pages": path.resolve(__dirname, "./src/app/pages"),
+            "@layouts": path.resolve(__dirname, "./src/app/layouts"),
+            
+        },
+    },
+});
